44e5083ab02e8e1b6991e77349ac49c1e1f7457d
Nawodawi <nawodawijebandara7@gmail.com>
2023-01-28 12:33:01 +0530

    pipeline file updated

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
         version = "=3.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform_rg"
+    storage_account_name = "blobfortf"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
